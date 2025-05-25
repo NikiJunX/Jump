a0663af08b2c3b639951a5e47fdb399a5a9a7305
NikiJunX <172074271+NikiJunX@users.noreply.github.com>
2025-05-25 13:19:15 +0300

    Update Jump.py

--- Jump.py
+++ Jump.py
@@ -1,5 +1,5 @@
 from pygame import *
-
+from random import randint
 
 window = display.set_mode((500,600))
 window.fill((68, 227, 219))
@@ -32,12 +32,43 @@ class Player(GameSprite):
             gs.rect.x -= 10
         if keys_pressed[K_d] and gs.rect.x < 400:
             gs.rect.x += 10
+        if keys_pressed[K_SPACE] and gs.rect.y > 1:
+            gs.rect.y -= 10
 
+lost = 0
+class Platform(GameSprite):
+    def __init__(self, player_speed, player_image, player_x, player_y):
+        super().__init__(player_speed, player_image, player_x, player_y)
+        self.image = transform.scale(image.load(player_image), (200, 220))
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+        if self.rect.y > 400:
+            self.rect.y = 0
+            self.rect.x = randint(80, 500 - 80)
+            lost += 1
 
 clock = time.Clock()
 fps = 60
 
+platforms = sprite.Group()
+
 gs = Player(5, 'player.png', 220, 250)
+
+plf = Platform(randint(2,3), 'plf.png', 210, 245)
+
+platf_1 = Platform(randint(1,2), 'plf.png', randint(100, 400), randint(10, 50))
+platf_2 = Platform(randint(1,2), 'plf.png', randint(100, 400), randint(10, 50))
+platf_3 = Platform(randint(1,2), 'plf.png', randint(100, 400), randint(10, 50))
+platf_4 = Platform(randint(1,2), 'plf.png', randint(100, 400), randint(10, 50))
+platf_5 = Platform(randint(1,2), 'plf.png', randint(100, 400), randint(10, 50))
+platf_6 = Platform(randint(1,2), 'plf.png', randint(100, 400), randint(10, 50))
+
+platforms.add(platf_1, platf_2, platf_3, platf_4, platf_5, platf_6)
 
 game = True
 while game:
@@ -53,6 +84,8 @@ while game:
     gs.reset()
     gs.update()
 
+    platforms.update()
+    platforms.draw(window)
 
 
 
