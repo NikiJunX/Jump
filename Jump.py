e3250115fa33ca011243020f1054bf3a88940de8
NikiJunX <172074271+NikiJunX@users.noreply.github.com>
2025-05-25 13:02:00 +0300

    Add files via upload

---
+++ Jump.py
@@ -0,0 +1,61 @@
+from pygame import *
+
+
+window = display.set_mode((500,600))
+window.fill((68, 227, 219))
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_speed, player_image, player_x, player_y):
+        super().__init__()
+        self.speed = player_speed
+        self.image = transform.scale(image.load(player_image),(95,75))
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def __init__(self, player_speed, player_image, player_x, player_y):
+        super().__init__(player_speed, player_image, player_x, player_y)
+        self.speed = player_speed
+        self.image = transform.scale(image.load(player_image),(50,50))
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_a] and gs.rect.x > 1:
+            gs.rect.x -= 10
+        if keys_pressed[K_d] and gs.rect.x < 400:
+            gs.rect.x += 10
+
+
+clock = time.Clock()
+fps = 60
+
+gs = Player(5, 'player.png', 220, 250)
+
+game = True
+while game:
+    window.fill((68, 227, 219))
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+
+
+
+    gs.reset()
+    gs.update()
+
+
+
+
+    clock.tick(fps)
+
+    display.update()
